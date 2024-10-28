0e06576f8fa8949541c6d473ab6fd6fd422d740e
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-10-28 16:47:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
-const MinHeap = require('minheap');
-
 function mincost(arr) {
-   
-    arr.sort((a,b)=>a-b)
-
-  
-
     let totalCost = 0;
 
     while (arr.length > 1) {
-        const first = arr[0];
-        const second = arr[1];
-        arr.shift()
-		arr.shift()
+        arr.sort((a, b) => a - b);
+
+        const first = arr.shift(); 
+        const second = arr.shift(); 
+
         const cost = first + second;
         totalCost += cost;
 
-       arr.push(cost);
-	  arr.sort((a,b)=>a-b)
+        arr.push(cost);
     }
 
     return totalCost;
