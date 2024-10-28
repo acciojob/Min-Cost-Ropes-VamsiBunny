12fa2ff6ee7fd03c07b1efb070e6ef962d8161d5
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-10-28 14:59:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-function mincost(arr) {
-   
-    const MinHeap = require('minheap');
+const MinHeap = require('minheap');
 
+function mincost(arr) {
     const heap = new MinHeap();
-    
     
     for (const length of arr) {
         heap.insert(length);
@@ -11,19 +9,13 @@ function mincost(arr) {
 
     let totalCost = 0;
 
-    
     while (heap.size() > 1) {
-     
         const first = heap.removeMin();
         const second = heap.removeMin();
 
-       
         const cost = first + second;
-
-     
         totalCost += cost;
 
-        
         heap.insert(cost);
     }
 
